bafaa98758de072b6877b224edf23344f3c3df9b
Geoffrey <geoffrey_mok2004@hotmail.com>
2023-07-27 10:53:34 +1000

    added to tests, entity factory, door, inventory and others

--- src/main/java/dungeonmania/entities/inventory/Inventory.java
+++ src/main/java/dungeonmania/entities/inventory/Inventory.java
@@ -11,110 +11,112 @@ import dungeonmania.entities.Player;
 import dungeonmania.entities.buildables.Bow;
 import dungeonmania.entities.collectables.Arrow;
 import dungeonmania.entities.collectables.Key;
+import dungeonmania.entities.collectables.SunStone;
 import dungeonmania.entities.collectables.Sword;
 import dungeonmania.entities.collectables.Treasure;
 import dungeonmania.entities.collectables.Wood;
 
 public class Inventory {
-    private List<InventoryItem> items = new ArrayList<>();
+        private List<InventoryItem> items = new ArrayList<>();
 
-    public boolean add(InventoryItem item) {
-        items.add(item);
-        return true;
-    }
-
-    public void remove(InventoryItem item) {
-        items.remove(item);
-    }
-
-    public List<String> getBuildables() {
-
-        int wood = count(Wood.class);
-        int arrows = count(Arrow.class);
-        int treasure = count(Treasure.class);
-        int keys = count(Key.class);
-        List<String> result = new ArrayList<>();
-
-        if (wood >= 1 && arrows >= 3) {
-            result.add("bow");
+        public boolean add(InventoryItem item) {
+                items.add(item);
+                return true;
         }
-        if (wood >= 2 && (treasure >= 1 || keys >= 1)) {
-            result.add("shield");
+
+        public void remove(InventoryItem item) {
+                items.remove(item);
         }
-        return result;
-    }
 
-    public InventoryItem checkBuildCriteria(Player p, boolean remove, boolean forceShield, EntityFactory factory) {
+        public List<String> getBuildables() {
 
-        List<Wood> wood = getEntities(Wood.class);
-        List<Arrow> arrows = getEntities(Arrow.class);
-        List<Treasure> treasure = getEntities(Treasure.class);
-        List<Key> keys = getEntities(Key.class);
+                int wood = count(Wood.class);
+                int arrows = count(Arrow.class);
+                int sunStones = count(SunStone.class);
+                int treasure = count(Treasure.class);
+                int keys = count(Key.class);
+                List<String> result = new ArrayList<>();
 
-        if (wood.size() >= 1 && arrows.size() >= 3 && !forceShield) {
-            if (remove) {
-                items.remove(wood.get(0));
-                items.remove(arrows.get(0));
-                items.remove(arrows.get(1));
-                items.remove(arrows.get(2));
-            }
-            return factory.buildBow();
-
-        } else if (wood.size() >= 2 && (treasure.size() >= 1 || keys.size() >= 1)) {
-            if (remove) {
-                items.remove(wood.get(0));
-                items.remove(wood.get(1));
-                if (treasure.size() >= 1) {
-                    items.remove(treasure.get(0));
-                } else {
-                    items.remove(keys.get(0));
+                if (wood >= 1 && arrows >= 3) {
+                        result.add("bow");
                 }
-            }
-            return factory.buildShield();
+                if (wood >= 2 && (treasure >= 1 || keys >= 1 || sunStones >= 1)) {
+                        result.add("shield");
+                }
+                return result;
         }
-        return null;
-    }
 
-    public <T extends InventoryItem> T getFirst(Class<T> itemType) {
-        for (InventoryItem item : items)
-            if (itemType.isInstance(item))
-                return itemType.cast(item);
-        return null;
-    }
+        public InventoryItem checkBuildCriteria(Player p, boolean remove, boolean forceShield, EntityFactory factory) {
 
-    public <T extends InventoryItem> int count(Class<T> itemType) {
-        int count = 0;
-        for (InventoryItem item : items)
-            if (itemType.isInstance(item))
-                count++;
-        return count;
-    }
+                List<Wood> wood = getEntities(Wood.class);
+                List<Arrow> arrows = getEntities(Arrow.class);
+                List<SunStone> sunStones = getEntities(SunStone.class);
+                List<Treasure> treasure = getEntities(Treasure.class);
+                List<Key> keys = getEntities(Key.class);
 
-    public Entity getEntity(String itemUsedId) {
-        for (InventoryItem item : items)
-            if (((Entity) item).getId().equals(itemUsedId))
-                return (Entity) item;
-        return null;
-    }
+                if (wood.size() >= 1 && arrows.size() >= 3 && !forceShield) {
+                        if (remove) {
+                                items.remove(wood.get(0));
+                                items.remove(arrows.get(0));
+                                items.remove(arrows.get(1));
+                                items.remove(arrows.get(2));
+                        }
+                        return factory.buildBow();
 
-    public List<Entity> getEntities() {
-        return items.stream().map(Entity.class::cast).collect(Collectors.toList());
-    }
+                } else if (wood.size() >= 2 && (treasure.size() >= 1 || keys.size() >= 1 || sunStones.size() >= 1)) {
+                        if (remove) {
+                                items.remove(wood.get(0));
+                                items.remove(wood.get(1));
+                                if (treasure.size() >= 1) {
+                                        items.remove(treasure.get(0));
+                                } else {
+                                        items.remove(keys.get(0));
+                                }
+                        }
+                        return factory.buildShield();
+                }
+                return null;
+        }
 
-    public <T> List<T> getEntities(Class<T> clz) {
-        return items.stream().filter(clz::isInstance).map(clz::cast).collect(Collectors.toList());
-    }
+        public <T extends InventoryItem> T getFirst(Class<T> itemType) {
+                for (InventoryItem item : items)
+                        if (itemType.isInstance(item))
+                                return itemType.cast(item);
+                return null;
+        }
 
-    public boolean hasWeapon() {
-        return getFirst(Sword.class) != null || getFirst(Bow.class) != null;
-    }
+        public <T extends InventoryItem> int count(Class<T> itemType) {
+                int count = 0;
+                for (InventoryItem item : items)
+                        if (itemType.isInstance(item))
+                                count++;
+                return count;
+        }
 
+        public Entity getEntity(String itemUsedId) {
+                for (InventoryItem item : items)
+                        if (((Entity) item).getId().equals(itemUsedId))
+                                return (Entity) item;
+                return null;
+        }
 
-    public BattleItem getWeapon() {
-        BattleItem weapon = getFirst(Sword.class);
-        if (weapon == null)
-            return getFirst(Bow.class);
-        return weapon;
-    }
+        public List<Entity> getEntities() {
+                return items.stream().map(Entity.class::cast).collect(Collectors.toList());
+        }
+
+        public <T> List<T> getEntities(Class<T> clz) {
+                return items.stream().filter(clz::isInstance).map(clz::cast).collect(Collectors.toList());
+        }
+
+        public boolean hasWeapon() {
+                return getFirst(Sword.class) != null || getFirst(Bow.class) != null;
+        }
+
+        public BattleItem getWeapon() {
+                BattleItem weapon = getFirst(Sword.class);
+                if (weapon == null)
+                        return getFirst(Bow.class);
+                return weapon;
+        }
 
 }
---
+++ src/test/java/dungeonmania/mvp/SunStoneTest.java
@@ -0,0 +1,69 @@
+package dungeonmania.mvp;
+
+import dungeonmania.DungeonManiaController;
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.util.Direction;
+import dungeonmania.util.Position;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SunStoneTest {
+        @Test
+        @Tag("17-1")
+        @DisplayName("Test player can use a sun stone to open and walk through a door")
+        public void useSunStoneWalkThroughOpenDoor() {
+                DungeonManiaController dmc;
+                dmc = new DungeonManiaController();
+                DungeonResponse res = dmc.newGame("d_sunStoneTest_unlockDoor", "c_sunStoneTest_unlockDoor");
+
+                // pick up sun stone
+                res = dmc.tick(Direction.RIGHT);
+                Position pos = TestUtils.getEntities(res, "player").get(0).getPosition();
+                assertEquals(1, TestUtils.getInventory(res, "sun_stone").size());
+
+                // walk through door and check sun stone stays
+                res = dmc.tick(Direction.RIGHT);
+                assertEquals(1, TestUtils.getInventory(res, "sun_stone").size());
+                assertNotEquals(pos, TestUtils.getEntities(res, "player").get(0).getPosition());
+        }
+
+        @Test
+        @Tag("17-2")
+        @DisplayName("Test player can use a sun stone to fulfill treasure goal")
+        public void sunStoneTreasureGoal() {
+                DungeonManiaController dmc;
+                dmc = new DungeonManiaController();
+                DungeonResponse res = dmc.newGame("d_sunStoneTest_treasureGoal", "c_sunStoneTest_treasureGoal");
+
+                // move player to right
+                res = dmc.tick(Direction.RIGHT);
+
+                // assert goal not met
+                assertTrue(TestUtils.getGoals(res).contains(":treasure"));
+
+                // collect treasure
+                res = dmc.tick(Direction.RIGHT);
+                assertEquals(1, TestUtils.getInventory(res, "sun_stone").size());
+
+                // assert goal not met
+                assertTrue(TestUtils.getGoals(res).contains(":treasure"));
+
+                // collect treasure
+                res = dmc.tick(Direction.RIGHT);
+                assertEquals(2, TestUtils.getInventory(res, "sun_stone").size());
+
+                // assert goal not met
+                assertTrue(TestUtils.getGoals(res).contains(":treasure"));
+
+                // collect treasure
+                res = dmc.tick(Direction.RIGHT);
+                assertEquals(3, TestUtils.getInventory(res, "sun_stone").size());
+
+                // assert goal met
+                assertEquals("", TestUtils.getGoals(res));
+        }
+
+}
