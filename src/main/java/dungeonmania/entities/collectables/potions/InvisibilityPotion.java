4bb5988b935bf00a3606c8d5380fc88007f01a16
Geoffrey <geoffrey_mok2004@hotmail.com>
2023-07-28 06:37:13 +1000

    got rid of magic numbers for buildables and potions

--- src/main/java/dungeonmania/entities/collectables/potions/InvisibilityPotion.java
+++ src/main/java/dungeonmania/entities/collectables/potions/InvisibilityPotion.java
@@ -4,21 +4,27 @@ import dungeonmania.battles.BattleStatistics;
 import dungeonmania.util.Position;
 
 public class InvisibilityPotion extends Potion {
-    public static final int DEFAULT_DURATION = 8;
+        public static final int DEFAULT_INVISIBLE_HEALTH = 0;
+        public static final int DEFAULT_INVISIBLE_ATTACK = 0;
+        public static final int DEFAULT_INVISIBLE_DEFENCE = 0;
+        public static final int DEFAULT_INVISIBLE_ATTACK_MAGNIFIER = 1;
+        public static final int DEFAULT_INVISIBLE_DAMAGEREDUCER = 1;
 
-    public InvisibilityPotion(Position position, int duration) {
-        super(position, duration);
-    }
+        public static final int DEFAULT_DURATION = 8;
 
-    @Override
-    public BattleStatistics applyBuff(BattleStatistics origin) {
-        return BattleStatistics.applyBuff(origin, createNewBattleStatistics());
-    }
+        public InvisibilityPotion(Position position, int duration) {
+                super(position, duration);
+        }
 
-    private BattleStatistics createNewBattleStatistics() {
-        return new BattleStatistics(0, 0, 0, 1, 1, false, false);
-    }
+        @Override
+        public BattleStatistics applyBuff(BattleStatistics origin) {
+                return BattleStatistics.applyBuff(origin, createNewBattleStatistics());
+        }
 
-
+        private BattleStatistics createNewBattleStatistics() {
+                return new BattleStatistics(DEFAULT_INVISIBLE_HEALTH, DEFAULT_INVISIBLE_ATTACK,
+                                DEFAULT_INVISIBLE_DEFENCE, DEFAULT_INVISIBLE_ATTACK_MAGNIFIER,
+                                DEFAULT_INVISIBLE_DAMAGEREDUCER, false, false);
+        }
 
 }
