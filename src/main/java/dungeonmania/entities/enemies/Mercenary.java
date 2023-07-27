8287172eaa0d02c02a82246a588db0be89188c88
Cedric Castelino <z5421684@ad.unsw.edu.au>
2023-07-28 05:04:20 +1000

    Add swamp_tile

--- src/main/java/dungeonmania/entities/enemies/Mercenary.java
+++ src/main/java/dungeonmania/entities/enemies/Mercenary.java
@@ -12,108 +12,112 @@ import dungeonmania.map.GameMap;
 import dungeonmania.util.Position;
 
 public class Mercenary extends Enemy implements Interactable {
-        public static final int DEFAULT_BRIBE_AMOUNT = 1;
-        public static final int DEFAULT_BRIBE_RADIUS = 1;
-        public static final double DEFAULT_ATTACK = 5.0;
-        public static final double DEFAULT_HEALTH = 10.0;
+    public static final int DEFAULT_BRIBE_AMOUNT = 1;
+    public static final int DEFAULT_BRIBE_RADIUS = 1;
+    public static final double DEFAULT_ATTACK = 5.0;
+    public static final double DEFAULT_HEALTH = 10.0;
 
-        private int bribeAmount = Mercenary.DEFAULT_BRIBE_AMOUNT;
-        private int bribeRadius = Mercenary.DEFAULT_BRIBE_RADIUS;
+    private int bribeAmount = Mercenary.DEFAULT_BRIBE_AMOUNT;
+    private int bribeRadius = Mercenary.DEFAULT_BRIBE_RADIUS;
 
-        private double allyAttack;
-        private double allyDefence;
-        private boolean allied = false;
-        private boolean isAdjacentToPlayer = false;
+    private double allyAttack;
+    private double allyDefence;
+    private boolean allied = false;
+    private boolean isAdjacentToPlayer = false;
 
-        public Mercenary(Position position, double health, double attack, int bribeAmount, int bribeRadius,
-                        double allyAttack, double allyDefence) {
-                super(position, health, attack);
-                this.bribeAmount = bribeAmount;
-                this.bribeRadius = bribeRadius;
-                this.allyAttack = allyAttack;
-                this.allyDefence = allyDefence;
+    public Mercenary(Position position, double health, double attack, int bribeAmount, int bribeRadius,
+            double allyAttack, double allyDefence) {
+        super(position, health, attack);
+        this.bribeAmount = bribeAmount;
+        this.bribeRadius = bribeRadius;
+        this.allyAttack = allyAttack;
+        this.allyDefence = allyDefence;
+    }
+
+    public boolean isAllied() {
+        return allied;
+    }
+
+    @Override
+    public void onOverlap(GameMap map, Entity entity) {
+        if (allied)
+            return;
+        super.onOverlap(map, entity);
+    }
+
+    /**
+     * check whether the current merc can be bribed
+     * @param player
+     * @return
+     */
+    private boolean canBeBribed(Player player) {
+        return bribeRadius >= 0 && player.countEntityOfType(Treasure.class) >= bribeAmount;
+    }
+
+    /**
+     * bribe the merc
+     */
+    private void bribe(Player player) {
+        for (int i = 0; i < bribeAmount; i++) {
+            player.use(Treasure.class);
         }
 
-        public boolean isAllied() {
-                return allied;
+    }
+
+    @Override
+    public void interact(Player player, Game game) {
+        allied = true;
+        bribe(player);
+        if (!isAdjacentToPlayer && Position.isAdjacent(player.getPosition(), getPosition()))
+            isAdjacentToPlayer = true;
+    }
+
+    private MovementStrategy movementStrategy;
+
+    public void setMovementStrategy(MovementStrategy movementStrategy) {
+        this.movementStrategy = movementStrategy;
+    }
+
+    @Override
+    public void move(Game game) {
+        Position nextPos;
+        GameMap map = game.getMap();
+        Player player = game.getPlayer();
+        if (allied) {
+            nextPos = isAdjacentToPlayer ? player.getPreviousDistinctPosition()
+                    : map.dijkstraPathFind(getPosition(), player.getPosition(), this);
+            if (!isAdjacentToPlayer && Position.isAdjacent(player.getPosition(), nextPos))
+                isAdjacentToPlayer = true;
+        } else if (player.getEffectivePotion() instanceof InvisibilityPotion) {
+            // Move random
+            this.movementStrategy = new RandomMovement();
+            nextPos = movementStrategy.move(this, game);
+            map.moveTo(this, nextPos);
+
+        } else if (player.getEffectivePotion() instanceof InvincibilityPotion) {
+            this.movementStrategy = new PlayerMovementStrategy();
+            Position offset = movementStrategy.move(this, game);
+            nextPos = offset;
+        } else {
+            // Follow hostile
+            nextPos = map.dijkstraPathFind(getPosition(), player.getPosition(), this);
         }
+        map.moveTo(this, nextPos);
+    }
 
-        @Override
-        public void onOverlap(GameMap map, Entity entity) {
-                if (allied)
-                        return;
-                super.onOverlap(map, entity);
-        }
+    @Override
+    public boolean isInteractable(Player player) {
+        return !allied && canBeBribed(player);
+    }
 
-        /**
-         * check whether the current merc can be bribed
-         * @param player
-         * @return
-         */
-        private boolean canBeBribed(Player player) {
-                return bribeRadius >= 0 && player.countEntityOfType(Treasure.class) >= bribeAmount;
-        }
+    @Override
+    public BattleStatistics getBattleStatistics() {
+        if (!allied)
+            return super.getBattleStatistics();
+        return new BattleStatistics(0, allyAttack, allyDefence, 1, 1);
+    }
 
-        /**
-         * bribe the merc
-         */
-        private void bribe(Player player) {
-                for (int i = 0; i < bribeAmount; i++) {
-                        player.use(Treasure.class);
-                }
-
-        }
-
-        @Override
-        public void interact(Player player, Game game) {
-                allied = true;
-                bribe(player);
-                if (!isAdjacentToPlayer && Position.isAdjacent(player.getPosition(), getPosition()))
-                        isAdjacentToPlayer = true;
-        }
-
-        private MovementStrategy movementStrategy;
-
-        public void setMovementStrategy(MovementStrategy movementStrategy) {
-                this.movementStrategy = movementStrategy;
-        }
-
-        @Override
-        public void move(Game game) {
-                Position nextPos;
-                GameMap map = game.getMap();
-                Player player = game.getPlayer();
-                if (allied) {
-                        nextPos = isAdjacentToPlayer ? player.getPreviousDistinctPosition()
-                                        : map.dijkstraPathFind(getPosition(), player.getPosition(), this);
-                        if (!isAdjacentToPlayer && Position.isAdjacent(player.getPosition(), nextPos))
-                                isAdjacentToPlayer = true;
-                } else if (player.getEffectivePotion() instanceof InvisibilityPotion) {
-                        // Move random
-                        this.movementStrategy = new RandomMovement();
-                        nextPos = movementStrategy.move(this, game);
-                        map.moveTo(this, nextPos);
-
-                } else if (player.getEffectivePotion() instanceof InvincibilityPotion) {
-                        this.movementStrategy = new PlayerMovementStrategy();
-                        Position offset = movementStrategy.move(this, game);
-                        nextPos = offset;
-                } else {
-                        // Follow hostile
-                        nextPos = map.dijkstraPathFind(getPosition(), player.getPosition(), this);
-                }
-                map.moveTo(this, nextPos);
-        }
-
-        @Override
-        public boolean isInteractable(Player player) {
-                return !allied && canBeBribed(player);
-        }
-
-        @Override
-        public BattleStatistics getBattleStatistics() {
-                if (!allied)
-                        return super.getBattleStatistics();
-                return new BattleStatistics(0, allyAttack, allyDefence, 1, 1);
-        }
+    public boolean isAdjacentToPlayer() {
+        return isAdjacentToPlayer;
+    }
 }
