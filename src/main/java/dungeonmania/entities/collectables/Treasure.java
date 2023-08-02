8a0b9501c57a2f254ee9a92259fcecf1bd3501e5
Carl Buchanan <carl.buchanan@student.unsw.edu.au>
2023-08-02 20:56:10 +1000

    automark_results

--- src/main/java/dungeonmania/entities/collectables/Treasure.java
+++
@@ -1,9 +0,0 @@
-package dungeonmania.entities.collectables;
-
-import dungeonmania.util.Position;
-
-public class Treasure extends Collectables {
-        public Treasure(Position position) {
-                super(position);
-        }
-}
--- src/main/java/dungeonmania/goals/states/GoalState.java
+++
@@ -1,9 +0,0 @@
-package dungeonmania.goals.states;
-
-import dungeonmania.Game;
-
-public interface GoalState {
-    boolean achieved(Game game);
-
-    String toString(Game game);
-}
